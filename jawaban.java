32a6ce90c575d531a997635c03234a26fd192f66
MFauzinurjaman <148457226+MFauzinurjaman@users.noreply.github.com>
2024-01-27 20:57:22 +0700

    Add files via upload

---
+++ jawaban.java
@@ -0,0 +1,30 @@
+// File: jawaban.java
+
+public class Jawaban {
+
+    public static void main(String[] args) {
+        // Tipe data numerik
+        int nilaiInt = 10;
+        long nilaiLong = 1000000000L;
+        float nilaiFloat = 3.14f;
+        double nilaiDouble = 2.71828;
+
+        // Tipe data karakter
+        char karakter = 'A';
+
+        // Tipe data boolean
+        boolean nilaiBoolean = true;
+
+        // Tipe data string
+        String teks = "Halo, ini contoh string.";
+
+        // Menampilkan nilai dari tipe data
+        System.out.println("Nilai dari tipe data int: " + nilaiInt);
+        System.out.println("Nilai dari tipe data long: " + nilaiLong);
+        System.out.println("Nilai dari tipe data float: " + nilaiFloat);
+        System.out.println("Nilai dari tipe data double: " + nilaiDouble);
+        System.out.println("Nilai dari tipe data char: " + karakter);
+        System.out.println("Nilai dari tipe data boolean: " + nilaiBoolean);
+        System.out.println("Nilai dari tipe data string: " + teks);
+    }
+}
